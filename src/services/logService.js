4ce85044583ede9e62c754d94267e4eb641975b9
Hugo Astier <hugoastier@Hugos-MacBook-Pro.local>
2020-01-22 16:01:58 +1300

    Rename logService_model to logService

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,6 +1,5 @@
 import * as Sentry from "@sentry/browser";
 
-// Create a Sentry account and copy your DSN here if you want to beneficiate from having logs in the cloud, and then remove '_model' to the file's name
 function init() {
   Sentry.init({
     dsn: "[YOUR_SENTRY_DSN_HERE]"
